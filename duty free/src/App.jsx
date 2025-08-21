6f4b40926d7fc45009225857ef96b6150e961ea2
Ganesh <134919900+kganesh09@users.noreply.github.com>
2025-08-21 13:33:00 +0530

    setup completed

---
+++ duty free/src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter as Router, Routes, Route, BrowserRouter,  } from 'react-router-dom';
+import './App.css'
+import Layout from './layouts/Layout';
+import Home from './pages/Home';
+import About from './pages/About';
+import NoPage from './pages/NoPage';
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index path="/" element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="*" element={<NoPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
