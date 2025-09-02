5582596b339593a9e56b85d547558de91f13b571
Ganesh <134919900+kganesh09@users.noreply.github.com>
2025-09-02 16:52:35 +0530

    user view and pagination completed

--- duty free/src/components/AdminMenuBar.jsx
+++ duty free/src/components/AdminMenuBar.jsx
@@ -2,8 +2,11 @@ import { Link, NavLink } from "react-router-dom"
 import "../Styles/adminmenu.css"
 import logo from "../assets/LOGO-Africa-Duty-free-def-WEB.png"
 import { RxDashboard } from "react-icons/rx";
-import { GiDrinkMe } from "react-icons/gi";
-import { MdCategory } from "react-icons/md";
+import { LuShoppingCart } from "react-icons/lu";
+import { LuUserRound } from "react-icons/lu";
+import { LuList } from "react-icons/lu";
+import { TiEquals } from "react-icons/ti";
+import { SiFluentbit } from "react-icons/si";
 
 const AdminMenuBar = () => {
     return (
@@ -11,12 +14,11 @@ const AdminMenuBar = () => {
             <Link to="/siteadmin" className='text-decoration-none px-3 py-3 d-block logo-wrapper'><img src={logo} alt="logo" className='admin-image' /></Link>
             <div className=" px-3 py-2 admin-menu-wrapper">
                 <NavLink to="/siteadmin" end className="d-flex rounded-2 align-items-center gap-3 admin-menu-link d-block text-decoration-none text-color-primary " > <RxDashboard style={{ width: "20px", height: "20px" }} /><span className="justuspro-regular">Dashboard</span></NavLink>
-                <NavLink to="/siteadmin/products" className="d-flex rounded-2 align-items-center gap-3 admin-menu-link d-block text-decoration-none text-color-primary " > <GiDrinkMe style={{ width: "20px", height: "20px" }} /><span className="justuspro-regular">Products</span></NavLink>
-                <NavLink to="/siteadmin/addcategory" className="d-flex rounded-2 align-items-center gap-3 admin-menu-link d-block text-decoration-none text-color-primary " ><MdCategory style={{ width: "20px", height: "20px" }} /><span className="justuspro-regular"> Add Categories</span></NavLink>
-                <NavLink to="/siteadmin/editcategory/:id" className="d-flex rounded-2 align-items-center gap-3 admin-menu-link d-block text-decoration-none text-color-primary " ><MdCategory style={{ width: "20px", height: "20px" }} /><span className="justuspro-regular"> Edit Categories</span></NavLink>
-                <NavLink to="/siteadmin/addsubcategory" end className="d-flex rounded-2 align-items-center gap-3 admin-menu-link d-block text-decoration-none text-color-primary " ><MdCategory style={{ width: "20px", height: "20px" }} /><span className="justuspro-regular">Add Sub Category</span></NavLink>
-                <NavLink to="/siteadmin/editsubcategory/:id" className="d-flex rounded-2 align-items-center gap-3 admin-menu-link d-block text-decoration-none text-color-primary " ><MdCategory style={{ width: "20px", height: "20px" }} /><span className="justuspro-regular">Edit Sub Category</span></NavLink>
-                <NavLink to="/siteadmin/listallcategories" className="d-flex rounded-2 align-items-center gap-3 admin-menu-link d-block text-decoration-none text-color-primary " ><MdCategory style={{ width: "20px", height: "20px" }} /><span className="justuspro-regular">List All Category</span></NavLink>
+                <NavLink to="/siteadmin/products" end className="d-flex rounded-2 align-items-center gap-3 admin-menu-link d-block text-decoration-none text-color-primary " > <LuShoppingCart style={{ width: "20px", height: "20px" }} /><span className="justuspro-regular">Products</span></NavLink>
+                <NavLink to="/siteadmin/listallcategories" end className="d-flex rounded-2 align-items-center gap-3 admin-menu-link d-block text-decoration-none text-color-primary " > <LuList style={{ width: "20px", height: "20px" }} /><span className="justuspro-regular">Categories</span></NavLink>
+                <NavLink to="/siteadmin/listallsubcategories" end className="d-flex rounded-2 align-items-center gap-3 admin-menu-link d-block text-decoration-none text-color-primary " > <TiEquals  style={{ width: "20px", height: "20px" }} /><span className="justuspro-regular">Sub-Categories</span></NavLink>
+                <NavLink to="/siteadmin/listallbrand" end className="d-flex rounded-2 align-items-center gap-3 admin-menu-link d-block text-decoration-none text-color-primary " > <SiFluentbit style={{ width: "20px", height: "20px" }} /><span className="justuspro-regular">Brands</span></NavLink>
+                <NavLink to="/siteadmin/users" end className="d-flex rounded-2 align-items-center gap-3 admin-menu-link d-block text-decoration-none text-color-primary " > <LuUserRound style={{ width: "20px", height: "20px" }} /><span className="justuspro-regular">users</span></NavLink>
             </div>
         </div>
     )
--- duty free/src/routes/AdminRoutes.jsx
+++ duty free/src/routes/AdminRoutes.jsx
@@ -7,6 +7,7 @@ import SubCategory from '../pages/AdminPages/addAndEditSubCategory'
 import ListCategories from '../pages/AdminPages/listAllCategories'
 import AddEditProducts from '../pages/AdminPages/AddEditProducts'
 import ViewProduct from '../pages/AdminPages/ViewProduct'
+import ViewUsers from '../pages/AdminPages/ViewUsers';
 
 const AdminRoutes = () => {
     return (
@@ -15,15 +16,15 @@ const AdminRoutes = () => {
                 <Route path="/" element={<AdminLayout />}>
                     <Route index path="/" element={<p>vicky</p>} />
                     <Route path="*" element={<NoPage />} />
-                    <Route path="addcategory" element={<AddCategory />} />
+                    <Route path="/addcategory" element={<AddCategory />} />
                     <Route path="/editcategory/:id" element={<AddCategory />} />
                     <Route path="/addsubcategory" element={<SubCategory />} />
                     <Route path="/editsubcategory/:id" element={<SubCategory />} />
                     <Route path="/listallcategories" element={<ListCategories />} />
-                    <Route index path="/products" element={<p>vicky</p>} />
                     <Route index path="/add-product" element={<AddEditProducts />} />
                     <Route index path="/edit-product/:id" element={<AddEditProducts />} />
-                    <Route index path="/view-product" element={<ViewProduct />} />
+                    <Route index path="/products" element={<ViewProduct />} />
+                    <Route index path="/users" element={<ViewUsers />} />
                 </Route>
             </Routes>
             <ToastContainer position="top-right" autoClose={2000} />
