36daa09f1c810d6f3817ac4b21499c70aa2574f3
Ganesh <134919900+kganesh09@users.noreply.github.com>
2025-09-08 10:43:52 +0530

    view user and view product page completed

--- duty free/src/pages/Wishlist.jsx
+++ duty free/src/pages/Wishlist.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import Common from '../commonMethod/Common'
 import ProductCard from '../components/commonComponents/ProductCard'
+import { Link } from 'react-router-dom'
 
 const Wishlist = () => {
     const [products, setproducts] = useState([])
@@ -12,16 +13,24 @@ const Wishlist = () => {
         };
         fetchproducts();
     }, [])
-    console.log(products);
 
     return (
-        <div>
-            <div className="product-grid container mx-auto">
-                {products?.map((Item, index) => (
-                   <ProductCard gridplacement={"product-grid"} hidePagnation={true} hidePopTool={true} imageheight={250} data={Item} />
-                ))}
-            </div>
-        </div>
+        <>
+            {products?.length === 0 ?
+                <>
+                    <div className='container p-5'>
+                        <p className='justuspro-medium'>Your wishlist is currently empty.</p>
+                        <Link className='justuspro-medium text-color-danger text-decoration-none' to={`/shop`}>Return to shop</Link>
+                    </div>
+                </> :
+                <>
+                    <div className="product-grid container mx-auto">
+                        {products?.map((Item, index) => (
+                            <ProductCard gridplacement={"product-grid"} hidePagnation={true} hidePopTool={true} imageheight={250} data={Item} />
+                        ))}
+                    </div>
+                </>}
+        </>
     )
 }
 
